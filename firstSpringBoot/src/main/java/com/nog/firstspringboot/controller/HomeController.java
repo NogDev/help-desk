5c93d43c0243c51c9992f856f0ec812fa0d91d33
nog <nog.guitar@gmail.com>
2019-12-16 18:40:22 -0300

    Using Spring security, but off

--- firstSpringBoot/src/main/java/com/nog/firstspringboot/controller/HomeController.java
+++ firstSpringBoot/src/main/java/com/nog/firstspringboot/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.nog.firstspringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HomeController {
 	@RequestMapping("/show")
 	public String show() {
 		return "Hello, first Spring Boot project";
+	}
+	
+	@GetMapping
+	public String index() {
+		return "Welcome!!!!";
 	}
 }
